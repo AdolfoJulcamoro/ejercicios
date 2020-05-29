75dc67ff74383bc12132ad992831a89fff3fdbcd
Adolfo Julcamoro <julcamorosm@gmail.com>
2020-05-29 12:28:26 -0500

    Update main.tf

--- jenkins/terraform_base/main.tf
+++ jenkins/terraform_base/main.tf
@@ -60,11 +60,6 @@ resource "google_compute_instance" "default" {
       // Include this section to give the VM an external ip address
     }
   }
-
-    //metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
-
-    // Apply the firewall rule to allow external IPs to access this instance
-    //tags = ["http-server"]
 }
 
 resource "google_compute_firewall" "http-server" {
@@ -76,9 +71,7 @@ resource "google_compute_firewall" "http-server" {
     ports    = ["22", "80", "8080"]
   }
 
-  // Allow traffic from everywhere to instances with an http-server tag
   source_ranges = ["0.0.0.0/0"]
-  //target_tags   = ["http-server"]
 }
 
 output "vm_ip" {
