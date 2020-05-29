367975fafebc0da84163ec18436f74b6c8a6e391
Adolfo Julcamoro <julcamorosm@gmail.com>
2020-05-29 12:23:35 -0500

    Update build.sh

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -135,7 +135,7 @@ set -e
 export PATH_BASE="$PWD"
 export PATH_ANSIBLE="$PATH_BASE"/ansible
 export PATH_TERRAFORM="$PATH_BASE"/terraform
-export SSH_KEY_PATH=/home/adolfo/.ssh/id_rsa_jenkins
+export SSH_KEY_PATH="${HOME}"/.ssh/id_rsa_jenkins
 export SSH_PUB_KEY_PATH="${SSH_KEY_PATH}.pub"
 export USER_NAME=`whoami`
 
@@ -143,105 +143,3 @@ show_menu
 read_option
 
 exit 0
-
-#Habilita las API de Compute Engine, Kubernetes Engine y Cloud Build
-#org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml
-
-#credentials        = "/home/adolfo/kube/creds/serviceaccount.json"
-#project_id         = "helical-sanctum-277902"
-#region             = "southamerica-east1"
-#zones              = ["southamerica-east1-a", "southamerica-east1-b", "southamerica-east1-c"]
-#service_account    = "terraform@helical-sanctum-277902.iam.gserviceaccount.com"
-#vm_name            = "vm-jenkins"
-#machine_type       = "f1-micro"
-#ssh_username       = "adolfo"
-#ssh_pub_key_path   = "~/.ssh/id_rsa_jenkins.pub"
-
-#remove with:\r\n  
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "35.198.8.45"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "34.95.201.51"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "35.198.46.114"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "34.95.180.25"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "34.95.224.23"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "34.95.201.51"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "35.198.46.114"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "34.95.203.195"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "35.198.8.45"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "34.95.224.23"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "34.95.180.25"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "35.198.8.45"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "35.198.46.114"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "34.95.201.51"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "34.95.203.195"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "104.197.158.127"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "104.197.158.127"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "35.239.209.189"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "104.197.158.127"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "35.239.209.189"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "104.197.158.127"
-#ssh-keygen -f "/home/adolfo/.ssh/known_hosts" -R "34.68.117.62"
-
-#Library
-# 	Name																																(firstlibrary)
-# 	Default version																											(master)
-#Currently maps to revision: db71c0ea113d2affed16320e8569f87e9fe6c635
-# 	Load implicitly																											(false)		
-# 	Allow default version to be overridden															(true)
-# 	Include @Library changes in job recent changes											(true)
-#Retrieval method
-# Modern SCM																														(true)
-#Source Code Management
-# Git																																	(chequed)
-# 	Project Repository	https://github.com/AdolfoJulcamoro/sharedlibhellomaven.git
-# 	Credentials	
-#
-#- none -
-# 
-# 	Behaviors	
-#Within Repository
-#Discover branches
-
-
-
-
-#[julcamorosm@vm-jenkins jenkins]$ ll
-#total 116
-#drwxr-xr-x.  3 jenkins jenkins   26 May 28 22:48 caches
-#-rw-r--r--.  1 jenkins jenkins  439 May 28 23:50 com.cloudbees.hudson.plugins.folder.config.AbstractFolderConfiguration.xml
-#-rw-r--r--.  1 jenkins jenkins 1673 May 28 23:50 config.xml
-#-rw-r--r--.  1 jenkins jenkins  156 May 28 22:37 hudson.model.UpdateCenter.xml
-#-rw-r--r--.  1 jenkins jenkins  443 May 28 23:50 hudson.plugins.git.GitSCM.xml
-#-rw-r--r--.  1 jenkins jenkins  370 May 28 22:37 hudson.plugins.git.GitTool.xml
-#-rw-r--r--.  1 jenkins jenkins  271 May 28 23:50 hudson.tasks.Mailer.xml
-#-rw-r--r--.  1 jenkins jenkins   76 May 28 23:50 hudson.tasks.Shell.xml
-#-rw-r--r--.  1 jenkins jenkins  216 May 28 23:50 hudson.triggers.SCMTrigger.xml
-#-rw-------.  1 jenkins jenkins 1712 May 28 22:34 identity.key.enc
-#drwxrwxr-x.  2 jenkins jenkins    6 May 28 22:35 init.groovy.d
-#-rw-r--r--.  1 jenkins jenkins    5 May 28 22:34 jenkins.install.InstallUtil.lastExecVersion
-#-rw-r--r--.  1 jenkins jenkins    5 May 28 22:34 jenkins.install.UpgradeWizard.state
-#-rw-r--r--.  1 jenkins jenkins  159 May 28 23:50 jenkins.model.ArtifactManagerConfiguration.xml
-#-rw-r--r--.  1 jenkins jenkins  253 May 28 23:50 jenkins.model.GlobalBuildDiscarderConfiguration.xml
-#-rw-r--r--.  1 jenkins jenkins  266 May 28 23:50 jenkins.model.JenkinsLocationConfiguration.xml
-#-rw-r--r--.  1 jenkins jenkins   86 May 28 23:50 jenkins.security.ResourceDomainConfiguration.xml
-#-rw-r--r--.  1 jenkins jenkins  171 May 28 22:34 jenkins.telemetry.Correlator.xml
-#drwxr-xr-x.  3 jenkins jenkins   23 May 28 22:38 jobs
-#drwxr-xr-x.  3 jenkins jenkins   19 May 28 22:34 logs
-#-rw-r--r--.  1 jenkins jenkins  907 May 28 22:37 nodeMonitors.xml
-#drwxr-xr-x.  2 jenkins jenkins    6 May 28 22:34 nodes
-#-rw-r--r--.  1 jenkins jenkins  272 May 28 23:50 org.jenkinsci.plugins.docker.workflow.declarative.GlobalConfig.xml
-#-rw-r--r--.  1 jenkins jenkins   46 May 28 22:55 org.jenkinsci.plugins.workflow.flow.FlowExecutionList.xml
-#-rw-r--r--.  1 jenkins jenkins  153 May 28 23:50 org.jenkinsci.plugins.workflow.flow.GlobalDefaultFlowDurabilityLevel.xml
-#-rw-r--r--.  1 jenkins jenkins 1861 May 28 23:50 org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml
-#-rw-r--r--.  1 jenkins jenkins  236 May 28 23:50 org.jenkins.plugins.lockableresources.LockableResourcesManager.xml
-#drwxr-xr-x. 58 jenkins jenkins 8192 May 28 22:37 plugins
-#-rw-r--r--.  1 jenkins jenkins  129 May 28 22:56 queue.xml
-#-rw-r--r--.  1 jenkins jenkins  129 May 28 22:37 queue.xml.bak
-#-rw-r--r--.  1 jenkins jenkins   64 May 28 22:34 secret.key
-#-rw-r--r--.  1 jenkins jenkins    0 May 28 22:34 secret.key.not-so-secret
-#drwx------.  4 jenkins jenkins 4096 May 28 22:48 secrets
-#drwxr-xr-x.  2 jenkins jenkins  100 May 28 22:37 updates
-#drwxr-xr-x.  2 jenkins jenkins   24 May 28 22:34 userContent
-#drwxr-xr-x.  3 jenkins jenkins   56 May 28 22:34 users
-#drwxr-xr-x.  2 jenkins jenkins    6 May 28 22:37 workflow-libs
-#drwxr-xr-x.  5 jenkins jenkins   66 May 28 22:48 workspace
-#[julcamorosm@vm-jenkins jenkins]$ 
